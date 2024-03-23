80dc930e9dbcb248fcf8bca58054a32a08fb490d
Leonides2 <leonidesart@hotmail.com>
2024-03-23 15:50:48 -0600

    5to-commit

--- src/quiz02/Content.tsx
+++ src/quiz02/Content.tsx
@@ -3,20 +3,20 @@ import MyContext from "../context/MyContext"
 
 const Content = () => {
 
-  const Product = useContext(MyContext)
+ const Product = useContext(MyContext)
  const desireProduct = Product.products.filter(item => item.step  === Product.step)
   
     
  return (
     <div className='card'>
 
-       {desireProduct.map(item =>
+       {desireProduct.map(item=>
         <>
           <p key={item.id}>Producto: {item.name}</p>
           <p key={item.id}>Precio: {item.price}</p>
         </>
        )
-       }
+}
     </div>
   )
 }
